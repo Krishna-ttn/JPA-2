56eb544df558084580fe9489e08dd63525cc88d5
Krishna Sharma <krishna.sharma@tothenew.com>
2025-03-14 17:08:52 +0530

    small changes in EmployeeRepository

--- src/main/java/com/krishna/jpa_2/Repository/EmployeeRepository.java
+++ src/main/java/com/krishna/jpa_2/Repository/EmployeeRepository.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -18,11 +19,14 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     //Update salary of all employees by a salary passed as a parameter whose
     //existing salary is less than the average salary.
+    @Query("SELECT AVG(e.salary) FROM Employee e")
+    Double findAverageSalary();
+
     @Modifying
-    @Transactional
-    @Query("UPDATE Employee e SET e.salary = :newSalary " +
-            "WHERE e.salary < (SELECT AVG(e2.salary) FROM Employee e2)")
-    int updateSalaryForEmployeesBelowAverage(double newSalary);
+    @Query("UPDATE Employee e SET e.salary = :newSalary WHERE e.salary < :avgSalary")
+        //@Param not necessary here
+    void updateSalaries(@Param("newSalary") Double newSalary, @Param("avgSalary") Double avgSalary);
+
 
     //Delete all employees with minimum salary.
     @Modifying
